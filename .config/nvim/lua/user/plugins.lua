edf0fec92c2d7f34b32ffa78874111409bd4c92d
Lukáš Koliandr <koliandr.lu@gmail.com>
2023-04-08 16:30:32 +0000

    feat(nvim): gitsigns keymap

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -47,12 +47,7 @@ return packer.startup(function(use)
 	})
 
 	-- git
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
+	use("lewis6991/gitsigns.nvim")
 
 	-- treesitter
 	use({
