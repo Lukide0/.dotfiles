35b8f4f3d0fce270d7e086f3943335883e09c2b1
Lukáš Koliandr <koliandr.lu@gmail.com>
2023-04-09 16:46:07 +0200

    feat: secondary monitor config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,4 +4,12 @@
 killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main &
+
+# Secondary monitor
+if [[ "$( xrandr -q | grep -w connected | wc -l )" -ne "1" ]]; then
+    # Get monitor name
+    monitor="$( xrandr -q | grep -w connected | tail -n 1 | awk '{print $1}' )"
+    
+    SECONDARY_MONITOR="$monitor" polybar secondary_bar &
+fi
