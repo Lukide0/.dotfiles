7999b4685d0fab3e319d45e022cd7ef8f9de75c1
Lukáš Koliandr <koliandr.lu@gmail.com>
2023-04-26 22:57:34 +0200

    fix(nvim): multiple encoding offsets

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -20,31 +20,33 @@ mason_null_ls.setup({ automatic_installation = false, handlers = {} })
 
 local null_ls = require("null-ls")
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.clang_format.with({
-            extra_args = { "-style=file" },
-        }),
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
+	sources = {
+		null_ls.builtins.formatting.clang_format.with({
+			extra_args = { "-style=file" },
+		}),
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local lspconfig = require("lspconfig")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 local function lspSymbol(name, icon)
-    local hl = "DiagnosticSign" .. name
-    vim.fn.sign_define(hl, { text = icon, numhl = "", texthl = hl })
+	local hl = "DiagnosticSign" .. name
+	vim.fn.sign_define(hl, { text = icon, numhl = "", texthl = hl })
 end
 
 lspSymbol("Error", "")
@@ -54,22 +56,31 @@ lspSymbol("Hint", "")
 
 local keymap = vim.keymap.set
 local lsp_on_attach = function()
-    keymap("n", "gs", vim.lsp.buf.hover, { buffer = 0 })                 -- show info
-    keymap("n", "gd", vim.lsp.buf.definition, { buffer = 0 })            -- go to definition
-    keymap("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })       -- go to type definition
-    keymap("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })        -- go to implementation
-    keymap("n", "gr", vim.lsp.buf.references, { buffer = 0 })            -- go to references
+	keymap("n", "gs", vim.lsp.buf.hover, { buffer = 0 }) -- show info
+	keymap("n", "gd", vim.lsp.buf.definition, { buffer = 0 }) -- go to definition
+	keymap("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 }) -- go to type definition
+	keymap("n", "gi", vim.lsp.buf.implementation, { buffer = 0 }) -- go to implementation
+	keymap("n", "gr", vim.lsp.buf.references, { buffer = 0 }) -- go to references
 
-    keymap("n", "<leader>da", vim.lsp.buf.code_action, { buffer = 0 })   -- show code actions
-    keymap("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })  -- go to next error
-    keymap("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })  -- go to previous error
-    keymap("n", "<leader>dl", ":Telescope diagnostics<cr>", { buffer = 0 }) -- list of errors
+	keymap("n", "<leader>da", vim.lsp.buf.code_action, { buffer = 0 }) -- show code actions
+	keymap("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 }) -- go to next error
+	keymap("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 }) -- go to previous error
+	keymap("n", "<leader>dl", ":Telescope diagnostics<cr>", { buffer = 0 }) -- list of errors
 
-    keymap("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
+	keymap("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
 end
 
 mason_lsp.setup_handlers({
-    function(server_name)
-        lspconfig[server_name].setup({ on_attach = lsp_on_attach })
-    end,
+	function(server_name)
+		-- temporary fix: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
+		if server_name == "clangd" then
+			lspconfig[server_name].setup({
+				on_attach = lsp_on_attach,
+				capabilities = cmp_nvim_lsp.default_capabilities(),
+				cmd = { "clangd", "--offset-encoding=utf-16" },
+			})
+		else
+			lspconfig[server_name].setup({ on_attach = lsp_on_attach })
+		end
+	end,
 })
