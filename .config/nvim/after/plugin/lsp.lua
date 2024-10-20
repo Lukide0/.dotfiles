4b8922661b2be21da2e3e8c6ee8f5dd04f2d40fc
Lukáš Koliandr <koliandr.lu@gmail.com>
2024-10-20 10:40:07 +0200

    feat!(nvim): removed null-ls.nvim

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,7 +2,6 @@
 local mason = require("mason")
 local mason_lsp = require("mason-lspconfig")
 local mason_dap = require("mason-nvim-dap")
-local mason_null_ls = require("mason-null-ls")
 local lsp_signature = require("lsp_signature")
 local neodev = require("neodev")
 
@@ -21,35 +20,9 @@ lsp_signature.setup({
 mason.setup()
 mason_dap.setup({ automatic_setup = true })
 mason_lsp.setup()
-mason_null_ls.setup({ automatic_installation = false, handlers = {} })
-
--- setup null_ls
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local null_ls = require("null-ls")
-null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.clang_format.with({
-			extra_args = { "-style=file" },
-		}),
-	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-})
 
 -- completion + icons
 local lspconfig = require("lspconfig")
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 local function lspSymbol(name, icon)
 	local hl = "DiagnosticSign" .. name
@@ -78,15 +51,66 @@ end
 -- mason_lsp setup
 mason_lsp.setup_handlers({
 	function(server_name)
-		-- temporary fix: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
-		if server_name == "clangd" then
-			lspconfig[server_name].setup({
-				on_attach = lsp_on_attach,
-				capabilities = cmp_nvim_lsp.default_capabilities(),
-				cmd = { "clangd", "--offset-encoding=utf-16", "--clang-tidy" },
-			})
-		else
-			lspconfig[server_name].setup({ on_attach = lsp_on_attach })
-		end
+		lspconfig[server_name].setup({ on_attach = lsp_on_attach })
 	end,
+})
+
+-- formatting on save
+local formatters = {}
+local formatters_by_ft = {}
+local known_bin = {
+	cmakelang = "cmake-format",
+}
+local formatter_config = {}
+
+local mason_registry = require("mason-registry")
+local conform = require("conform")
+
+for _, pkg in pairs(mason_registry.get_installed_packages()) do
+	for _, type in pairs(pkg.spec.categories) do
+		if type ~= "Formatter" then
+			goto continue
+		end
+
+		local config = conform.get_formatter_config(pkg.spec.name)
+
+		-- not supported formatter
+		if not config then
+			local bin = known_bin[pkg.spec.name]
+
+			if not bin then
+				goto continue
+			end
+
+			config = {
+				command = bin,
+				args = { "$FILENAME" },
+				stdin = true,
+				require_cwd = false,
+			}
+		end
+
+		local config_func = formatter_config[pkg.spec.name]
+
+		formatters[pkg.spec.name] = config_func and config_func(config) or config
+
+		for _, file_type in pairs(pkg.spec.languages) do
+			local ft = string.lower(file_type)
+
+			formatters_by_ft[ft] = formatters_by_ft[ft] or {}
+			table.insert(formatters_by_ft[ft], pkg.spec.name)
+		end
+
+		::continue::
+	end
+end
+
+conform.setup({
+	formatters_by_ft = formatters_by_ft,
+	formatters = formatters,
+
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "last",
+	},
 })
--- .config/nvim/lua/user/plugins/lsp.lua
+++ .config/nvim/lua/user/plugins/lsp.lua
@@ -1,11 +1,6 @@
 return {
-	"neovim/nvim-lspconfig", -- Configurations for Nvim LSP
-	"williamboman/mason.nvim", -- package manager
-	"williamboman/mason-lspconfig.nvim",
-	"jay-babu/mason-nvim-dap.nvim",
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	"jay-babu/mason-null-ls.nvim",
+    "neovim/nvim-lspconfig", -- Configurations for Nvim LSP
+    "williamboman/mason.nvim", -- package manager
+    "williamboman/mason-lspconfig.nvim",
+    "stevearc/conform.nvim",
 }
